bea9abfe6daca8bf7286ab3045fd291df51c4742
John <antarcticdepths@gmail.com>
2023-06-19 13:40:44 +0200

    chunker app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { alarm, alarmFill, alignBottom } from 'ngx-bootstrap-icons';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ListMakerComponent } from './list-maker/list-maker.component';
 import { ChunkComponent } from './chunk-list/chunk/chunk.component';
 import { ChunkListComponent } from './chunk-list/chunk-list.component';
+
+const icons = {
+  alarm,
+  alarmFill,
+  alignBottom,
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +22,7 @@ import { ChunkListComponent } from './chunk-list/chunk-list.component';
     ListMakerComponent,
     ChunkListComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgxBootstrapIconsModule.pick(icons)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/chunk-list/chunk/chunk.component.ts
+++ src/app/chunk-list/chunk/chunk.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { alarm, alarmFill, alignBottom } from 'ngx-bootstrap-icons';
 
 @Component({
   selector: 'app-chunk',
