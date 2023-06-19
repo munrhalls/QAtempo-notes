90795bece00df272ddf339a51e4c887ae1b1263f
John <antarcticdepths@gmail.com>
2023-06-19 14:45:24 +0200

    chunker app

--- src/app/chunk-list/chunk-list.component.ts
+++ src/app/chunk-list/chunk-list.component.ts
@@ -8,7 +8,15 @@ import { Component } from '@angular/core';
 export class ChunkListComponent {
   listNames = ['List a', 'List b'];
   lists = [
-    [{ question: 'Letter?', answer: 'a' }],
-    [{ question: 'Letter?', answer: 'b' }],
+    [
+      { question: 'Letter?', answer: 'a' },
+      { question: 'Letter?', answer: 'b' },
+      { question: 'Letter?', answer: 'c' },
+    ],
+    [
+      { question: 'Letter?', answer: 'd' },
+      { question: 'Letter?', answer: 'f' },
+      { question: 'Letter?', answer: 'g' },
+    ],
   ];
 }
