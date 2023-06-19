a51fa4b11433f5601652dba48f758b548d41cb49
John <antarcticdepths@gmail.com>
2023-06-19 12:22:49 +0200

    chunker app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ChunkComponent } from './chunk/chunk.component';
 import { ListMakerComponent } from './list-maker/list-maker.component';
+import { ChunkComponent } from './chunk-list/chunk/chunk.component';
+import { ChunkListComponent } from './chunk-list/chunk-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ChunkComponent, ListMakerComponent],
+  declarations: [
+    AppComponent,
+    ChunkComponent,
+    ListMakerComponent,
+    ChunkListComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/chunk-list/chunk-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chunk-list',
+  templateUrl: './chunk-list.component.html',
+  styleUrls: ['./chunk-list.component.scss']
+})
+export class ChunkListComponent {
+
+}
