2c02ac95ae94286e1a3128bb8398f5b8b065d367
John <antarcticdepths@gmail.com>
2023-06-22 19:37:40 +0200

    chunks app!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,6 +31,41 @@ export class AppComponent {
       'intro'
     ),
   ];
-
+  addNewChunk() {
+    this.chunks.push(
+      new Chunk(
+        'Exam title..',
+        [
+          {
+            question: 'Question 1. How to make an exam in here?',
+            answer:
+              'Click the "EDIT" button above, and just fill in the questions & answers!',
+          },
+          {
+            question: 'Question 2. Okay, how to then take exam I have made?',
+            answer:
+              'After you have edited your questions & answers, simply click the "BACK TO DISPLAY" button and go at it! Then hit the "SUBMIT" button.',
+          },
+          {
+            question: 'Question 3. How about adding and deleting exams?',
+            answer: 'Simply click the "ADD EXAM" or "DELETE EXAM" buttons!',
+          },
+          {
+            question: 'Question 4. How does it work?',
+            answer:
+              'It is as if you made a list of questions and answers, and then you take on the questions without looking at the answers. When you are done, you check your answers with the list of proper answers.',
+          },
+          {
+            question: 'Question 5. Now what?',
+            answer:
+              'Just delete these question & answer points, and make your own list!',
+          },
+        ],
+        'beginner',
+        'display',
+        'intro'
+      )
+    );
+  }
   title = 'exam-yourself';
 }
