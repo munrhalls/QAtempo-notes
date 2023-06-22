b9e2f49443437591abaae46669fa0edd047ab030
John <antarcticdepths@gmail.com>
2023-06-22 08:41:12 +0200

    chunks app!

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -11,9 +11,4 @@ export class EditComponent {
     list: { question: string; answer: string }[];
     mode: string;
   };
-  deletePoint(point: { question: string; answer: string }) {
-    this.chunk.list = this.chunk.list.filter(
-      (item) => item.question !== point.question
-    );
-  }
 }
